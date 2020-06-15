99434c81ccad2b0b0bfc4783229f8d7d8de185bb
George Gabra <george-gabra@msn.com>
2020-06-15 16:47:35 +0200

    Add files via upload

---
+++ UdagramCreateStack.sh
@@ -0,0 +1,156 @@
+#!/bin/bash
+
+function CreateStack ()
+{
+
+#Validate CloudFormation YAML Template
+#Creation script will be terminated in case of template error to avoid creation of SSH Keys and SSM Parameters 
+echo -e "\e[1;32mTask1:\e[0m"
+echo "Validating CloudFormation Template file $TemplateFile...."
+if aws cloudformation validate-template --template-body file://$TemplateFile; then
+    echo "Template file $TemplateFile is valid"
+else
+   echo -e "\e[1;31mError:\e[0m Template file $TemplateFile is not valid"
+   exit
+fi
+
+#Create EC2 SSH Key Pair in AWS selected region
+#Query the SSH key and add it to user's SSH directory
+echo ""
+echo -e "\e[1;32mTask2:\e[0m"
+echo "Creating SSH Key pair on AWS Region $AWSRegion...."
+SSHKeyName="${StackName}-SSHKey"
+SSHKeyPair=`aws ec2 create-key-pair --key-name $SSHKeyName --query 'KeyMaterial' --output text --region $AWSRegion`
+
+echo ""
+echo -e "\e[1;32mTask3:\e[0m"
+echo "Copying the SSH key to your local SSH directory. File name is $HOME/.ssh/$SSHKeyName.pem"
+echo -e "${SSHKeyPair//_/\\n}" > $HOME/.ssh/$SSHKeyName.pem
+chmod 600 $HOME/.ssh/$SSHKeyName.pem
+echo ""
+
+#Get Ubuntu server 18.04 Image AMI from the selected AWS region
+UbuntuImageID=`aws ec2  describe-images  --filters "Name=name,Values=ubuntu/images/hvm-ssd/ubuntu-bionic-18.04-amd64-server-20200408" "Name=architecture,Values=x86_64"  "Name=root-device-type,Values=ebs" "Name=virtualization-type,Values=hvm"   --query 'Images[*].[ImageId]' --output text --region $AWSRegion`
+
+#Create AWS SSM Parameters in the selected Region 
+/bin/echo -e "\e[1;32mTask4:\e[0m"
+echo "Creating SSM Parameters on AWS Region $AWSRegion...."
+aws ssm put-parameter --name /Dev/Udagram/EnvironmentName --value "Udagram-Dev-George" --type String --overwrite --region $AWSRegion
+aws ssm put-parameter --name /Dev/Udagram/S3BucketName --value "udagram-dev-code" --type String --overwrite --region $AWSRegion
+aws ssm put-parameter --name /Dev/Udagram/AWSManagedPolicyARNForS3 --value "arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess" --type String --overwrite --region $AWSRegion
+aws ssm put-parameter --name /Dev/Udagram/AWSManagedPolicyARNForCF --value "arn:aws:iam::aws:policy/AWSCloudFormationReadOnlyAccess" --type String --overwrite --region $AWSRegion
+aws ssm put-parameter --name /Dev/Udagram/AWSManagedPolicyARNForSSM --value "arn:aws:iam::aws:policy/AmazonSSMReadOnlyAccess" --type String --overwrite --region $AWSRegion
+aws ssm put-parameter --name /Dev/Udagram/VpcCIDR --value "10.0.0.0/16" --type String --overwrite --region $AWSRegion
+aws ssm put-parameter --name /Dev/Udagram/PublicSubnet1CIDR --value "10.0.0.0/24" --type String --overwrite --region $AWSRegion
+aws ssm put-parameter --name /Dev/Udagram/PublicSubnet2CIDR --value "10.0.1.0/24" --type String --overwrite --region $AWSRegion
+aws ssm put-parameter --name /Dev/Udagram/PrivateSubnet1CIDR --value "10.0.2.0/24" --type String --overwrite --region $AWSRegion
+aws ssm put-parameter --name /Dev/Udagram/PrivateSubnet2CIDR --value "10.0.3.0/24" --type String --overwrite --region $AWSRegion
+aws ssm put-parameter --name /Dev/Udagram/WebAppImageID --value "$UbuntuImageID" --type String --overwrite --region $AWSRegion
+aws ssm put-parameter --name /Dev/Udagram/LinuxSSHKey --value "$SSHKeyName" --type String --overwrite --region $AWSRegion
+aws ssm put-parameter --name /Dev/Udagram/SSHPrivateKey --type SecureString --value "$SSHKeyPair" --overwrite --region $AWSRegion
+aws ssm put-parameter --name /Dev/Udagram/WebAppInstanceType --value "t3.micro" --type String --overwrite --region $AWSRegion
+aws ssm put-parameter --name /Dev/Udagram/WebAppDiskSize --value "10" --type String --overwrite --region $AWSRegion
+aws ssm put-parameter --name /Dev/Udagram/BastionHostImageID --value "$UbuntuImageID" --type String --overwrite --region $AWSRegion
+aws ssm put-parameter --name /Dev/Udagram/BastionHostInstanceType --value "t3.micro" --type String --overwrite --region $AWSRegion
+aws ssm put-parameter --name /Dev/Udagram/BastionHostDiskSize --value "10" --type String --overwrite --region $AWSRegion
+
+#Create environment parameters file which will be used during cleanup
+echo $StackName >  ./udagram-dev-variables.txt
+echo $AWSRegion >>  ./udagram-dev-variables.txt
+echo $SSHKeyName >>  ./udagram-dev-variables.txt
+
+#Create the CloudFormation Stack on the selected Region
+CFCreateStackCMD="aws cloudformation create-stack --stack-name $StackName --template-body file://$TemplateFile --capabilities "CAPABILITY_IAM" "CAPABILITY_NAMED_IAM" --region $AWSRegion";
+echo ""
+/bin/echo -e "\e[1;32mTask5:\e[0m"
+echo "Creating Cloud Formation Stack $StackName on AWS Region $AWSRegion...."
+eval $CFCreateStackCMD
+echo "Creating Cloud Formation Stack $StackName on AWS Region $AWSRegion has been initiated"
+echo ""
+echo -e "To Monitor stack creation events run command:\e[1;34m aws cloudformation describe-stacks --stack-name $StackName --region $AWSRegion --query \"Stacks[0].[StackName, StackStatus]\" --output text \e[0m"
+echo ""
+echo -e "To show stack outputs when creation completes run command:\e[1;34m aws cloudformation describe-stacks --stack-name $StackName --region $AWSRegion --query Stacks[0].Outputs[*] --output table \e[0m" 
+echo ""
+/bin/echo -e "\e[1;32mDone\e[0m"
+}
+
+#Get the Stack Name from the User
+printf "Enter CloudFormation Stack Name to be created on AWS: "
+read StackName
+
+#Get Stack Template File Name
+printf "Enter CloudFormation Template File Name: "
+read TemplateFile
+
+#Menu to select the AWS Region on which the environment will be created 
+echo 'Select AWS Region to create the environment: '
+AWSAllowedRegions=("US East (Ohio)" "US East (N. Virginia)" "US West (N. California)" "US West (Oregon)" "Canada (Central)" "Africa (Cape Town)" "Asia Pacific (Hong Kong)"  "China (Beijing)" "Europe (Frankfurt)" "Europe (Ireland)" "Middle East (Bahrain)" "South America (São Paulo)" "Quit" )
+select UserRegion in "${AWSAllowedRegions[@]}"
+do
+    case $UserRegion in
+        "US East (Ohio)")
+                AWSRegion="us-east-2"
+                CreateStack $StackName $TemplateFile $AWSRegion
+                break
+            ;;
+        "US East (N. Virginia)")
+                AWSRegion="us-east-1"
+                CreateStack $StackName $TemplateFile $AWSRegion
+                break
+            ;;
+        "US West (N. California)")
+                AWSRegion="us-west-1"
+                CreateStack $StackName $TemplateFile $AWSRegion
+                break
+            ;;
+        "US West (Oregon)")
+                AWSRegion="us-west-2"
+                CreateStack $StackName $TemplateFile $AWSRegion
+                break
+            ;;
+        "Canada (Central)")
+                AWSRegion="ca-central-1"
+                CreateStack $StackName $TemplateFile $AWSRegion
+                break
+            ;;
+        "Africa (Cape Town)")
+                AWSRegion="af-south-1"
+                CreateStack $StackName $TemplateFile $AWSRegion
+                break
+            ;;
+        "Asia Pacific (Hong Kong)")
+                AWSRegion="ap-east-1"
+                CreateStack $StackName $TemplateFile $AWSRegion
+                break
+            ;;
+        "China (Beijing)")
+                AWSRegion="cn-north-1"
+                CreateStack $StackName $TemplateFile $AWSRegion
+                break
+            ;;
+        "Europe (Frankfurt)")
+                AWSRegion="eu-central-1"
+                CreateStack $StackName $TemplateFile $AWSRegion
+                break
+            ;;
+        "Europe (Ireland)")
+                AWSRegion="eu-west-1"
+                CreateStack $StackName $TemplateFile $AWSRegion
+                break
+            ;;
+        "Middle East (Bahrain)")
+                AWSRegion="me-south-1"
+                CreateStack $StackName $TemplateFile $AWSRegion
+                break
+            ;;
+        "South America (São Paulo)")
+                AWSRegion="sa-east-1"
+                CreateStack $StackName $TemplateFile $AWSRegion
+                break
+            ;;
+        "Quit")
+            break
+            ;;
+        *) echo "Invalid Region  $REPLY";;
+    esac
+done
---
+++ UdagramDeleteStack.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+#Read environment parameters from udagram-dev-variables.txt file and add them to array
+EnvValuesArr=()
+while IFS= read -r line; do
+   EnvValuesArr+=("$line")
+done < ./udagram-dev-variables.txt
+
+#Empty S3 Buket
+echo ""
+echo -e "\e[1;32mTask1:\e[0m"
+echo "Deleting S3 Bucket objects...."
+#Get S3 Bucket Name from the stack resources
+StackS3BuckName=`aws cloudformation describe-stack-resources --stack-name ${EnvValuesArr[0]} --logical-resource-id S3Bucket --query "StackResources[0].PhysicalResourceId" --output text`
+aws s3 rm s3://$StackS3BuckName --recursive
+
+#Delete CloudFormation Stack
+echo ""
+echo -e "\e[1;32mTask2:\e[0m"
+echo "Deleting CloudFormation Stack ${EnvValuesArr[0]} from AWS Region ${EnvValuesArr[1]}...."
+aws cloudformation delete-stack --stack-name ${EnvValuesArr[0]} --region ${EnvValuesArr[1]}
+
+echo ""
+echo "Deleting Cloud Formation Stack ${EnvValuesArr[0]} on AWS Region ${EnvValuesArr[1]} has been initiated"
+echo -e "To Monitor stack deletion events run command:\e[1;34m aws cloudformation describe-stacks --stack-name ${EnvValuesArr[0]} --region ${EnvValuesArr[1]} --query \"Stacks[0].[StackName, StackStatus]\" --output text \e[0m"
+
+#Delete the SSH Key Pair
+echo ""
+echo -e "\e[1;32mTask3:\e[0m"
+echo "Deleting SSH Key Pair from AWS Region ${EnvValuesArr[1]}...."
+aws ec2 delete-key-pair --key-name ${EnvValuesArr[2]} --region ${EnvValuesArr[1]}
+
+#Delete the SSH key from user's SSH directory
+echo ""
+echo -e "\e[1;32mTask4:\e[0m"
+echo "Deleting SSH key ${EnvValuesArr[2]}.pem from $HOME/.ssh directory...."
+rm $HOME/.ssh/${EnvValuesArr[2]}.pem
+
+#Delete AWS SSM Parameters
+echo "Deleting AWS SSM Parameters from AWS Region ${EnvValuesArr[1]}...."
+aws ssm delete-parameter --name /Dev/Udagram/EnvironmentName --region ${EnvValuesArr[1]}
+aws ssm delete-parameter --name /Dev/Udagram/S3BucketName --region ${EnvValuesArr[1]}
+aws ssm delete-parameter --name /Dev/Udagram/AWSManagedPolicyARNForS3 --region ${EnvValuesArr[1]}
+aws ssm delete-parameter --name /Dev/Udagram/AWSManagedPolicyARNForCF --region ${EnvValuesArr[1]}
+aws ssm delete-parameter --name /Dev/Udagram/AWSManagedPolicyARNForSSM --region ${EnvValuesArr[1]}
+aws ssm delete-parameter --name /Dev/Udagram/VpcCIDR --region ${EnvValuesArr[1]}
+aws ssm delete-parameter --name /Dev/Udagram/PublicSubnet1CIDR --region ${EnvValuesArr[1]}
+aws ssm delete-parameter --name /Dev/Udagram/PublicSubnet2CIDR --region ${EnvValuesArr[1]}
+aws ssm delete-parameter --name /Dev/Udagram/PrivateSubnet1CIDR --region ${EnvValuesArr[1]}
+aws ssm delete-parameter --name /Dev/Udagram/PrivateSubnet2CIDR --region ${EnvValuesArr[1]}
+aws ssm delete-parameter --name /Dev/Udagram/WebAppImageID --region ${EnvValuesArr[1]}
+aws ssm delete-parameter --name /Dev/Udagram/LinuxSSHKey --region ${EnvValuesArr[1]}
+aws ssm delete-parameter --name /Dev/Udagram/SSHPrivateKey --region ${EnvValuesArr[1]}
+aws ssm delete-parameter --name /Dev/Udagram/WebAppInstanceType --region ${EnvValuesArr[1]}
+aws ssm delete-parameter --name /Dev/Udagram/WebAppDiskSize --region ${EnvValuesArr[1]}
+aws ssm delete-parameter --name /Dev/Udagram/BastionHostImageID --region ${EnvValuesArr[1]}
+aws ssm delete-parameter --name /Dev/Udagram/BastionHostInstanceType --region ${EnvValuesArr[1]}
+aws ssm delete-parameter --name /Dev/Udagram/BastionHostDiskSize --region ${EnvValuesArr[1]}
+
+#Delete environment parameters file
+echo ""
+echo -e "\e[1;32mTask5:\e[0m"
+echo "Deleting environment parameters file...."
+rm ./udagram-dev-variables.txt
+
+echo ""
+echo -e "\e[1;32mDone\e[0m"
